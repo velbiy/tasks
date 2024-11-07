abebf08c731e18ef333d46836e9841a9e5f96b35
velbiy <v.beklemyshev1205@gmail.com>
2024-11-07 14:49:37 +0300

    Add files via upload

---
+++ task_module_2_2.py
@@ -0,0 +1,26 @@
+# 1 variant
+first = int(input())
+second = int(input())
+third = int(input())
+if first == second and first == third:
+    print(3)
+else:
+    if first == second or first == third or second == third:
+        print(2)
+    else:
+        print(0)
+
+
+# 2 variant
+# first = int(input())
+# second = int(input())
+# third = int(input())
+# if first == second and first == third:
+#     print(3)
+# elif first == second or first == third or second == third:
+#     print(2)
+# else:
+#     print(0)
+
+
+
